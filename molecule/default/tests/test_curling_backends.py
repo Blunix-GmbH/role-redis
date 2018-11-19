8004328bc4ff0b5d5f26c7e564a373bae8862f09
Peter Thurner <p.thurner@blunix.org>
2018-11-19 17:46:19 +0100

    fixed password requests

---
+++ molecule/default/tests/test_curling_backends.py
@@ -0,0 +1,10 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_password_auth_enabled(host):
+    r = host.run('echo "config get requirepass" | redis-cli -h 127.0.0.1 -p 6379 -a secret | grep requirepass')
+    assert 'requirepass' == r.stdout
